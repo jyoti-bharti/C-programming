c7ee8967f9f7f7c95b71812a332216f472c3b1e3
jyoti-bharti <er.raviraja.rr@gmail.com>
2020-07-13 13:45:47 +0530

    2nd maximum no.

---
+++ 2ndmax.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,k=0;
+	printf("enter no. of elements:");
+	scanf("%d",&n);
+
+	int a[n];
+	printf("\nenter elements :");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+
+	int max[n];
+	for(i=0;i<n;i++)
+	{
+		max[i]=0;
+	}
+
+
+	max[0]=a[0];
+	for(i=0;i<n;i++)
+	{
+		if(a[i]>max[k])
+		{
+			++k;
+			max[k]=a[i];
+		}
+	}
+	printf("\nmax. element:%d",max[k]);
+	printf("\n2nd max. element:%d",max[k-1]);
+}
